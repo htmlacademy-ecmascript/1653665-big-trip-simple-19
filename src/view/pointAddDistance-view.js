56d060522a7be7a2e84e0ec384991af2337ecb8c
Deltaqw <109920316+Deltaqw@users.noreply.github.com>
2023-01-20 19:25:45 +0300

    Update pointAddDistance-view.js

--- src/view/pointAddDistance-view.js
+++ src/view/pointAddDistance-view.js
@@ -2,6 +2,7 @@ import {createElement} from '../render.js';
 
 function createFilterTemplate() {
   return `
+    <li class="trip-events__item">
   <form class="event event--edit" action="#" method="post">
 <header class="event__header">
 <div class="event__type-wrapper">
@@ -148,6 +149,7 @@ event__field-group--price">
 </section>
 </section>
 </form>
+</li>
   `;
 }
 
